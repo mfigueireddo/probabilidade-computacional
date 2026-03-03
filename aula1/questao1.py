02d6a1df848bb961d58a6f91f8a633a6a50d1942
Matheus Figueiredo <108003682+mfigueireddo@users.noreply.github.com>
2026-03-03 20:49:51 -0300

    Fixed bugs lead by copy pasting the code

--- aula1/questao1.py
+++ aula1/questao1.py
@@ -7,38 +7,38 @@ dice_options = {"1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0}
 dice_chances = [ 1 / len(dice_options) ] * len(dice_options)
 
 combined_options = {
-f"{coin} & {dice}" : 0
-for coin in coin_options
-for dice in dice_options
+    f"{coin} & {dice}" : 0
+    for coin in coin_options
+    for dice in dice_options
 }
 
 samples_amount = 1_000_000
 
 coin_results = random.choices(
-population=list(coin_options.keys()),
-weights=coin_chances,
-k=samples_amount
+    population=list(coin_options.keys()),
+    weights=coin_chances,
+    k=samples_amount
 )
 
 dice_results = random.choices(
-population=list(dice_options.keys()),
-weights=dice_chances,
-k=samples_amount
+    population=list(dice_options.keys()),
+    weights=dice_chances,
+    k=samples_amount
 )
 
 for coin_result, dice_result in zip(coin_results, dice_results):
-coin_options[coin_result] += 1
-dice_options[dice_result] += 1
-combined_options[f"{coin_result} & {dice_result}"] += 1
+    coin_options[coin_result] += 1
+    dice_options[dice_result] += 1
+    combined_options[f"{coin_result} & {dice_result}"] += 1
 
 print("Coin results:")
 for key, value in coin_options.items():
-print(f"{key}: {value}")
+    print(f"{key}: {value}")
 
 print("\nDice results:")
 for key, value in dice_options.items():
-print(f"{key}: {value}")
+    print(f"{key}: {value}")
 
 print("\nCombined results:")
 for key, value in combined_options.items():
-print(f"{key}: {value}")
+    print(f"{key}: {value}")
--- aula1/questao2.py
+++ aula1/questao2.py
@@ -1,13 +1,15 @@
+import random
+
 # Cálculo das probabilidades em cada caso
 
 cases = {"A": 0, "B": 0}
 
 def contabilize_results(cases, results):
 
-first, second, third = results[:3]
+    first, second, third = results[:3]
 
-if (first == "Cara"):
-cases["A"] += 1
+    if (first == "Cara"):
+        cases["A"] += 1
 
 # Opções e chances
 
@@ -19,9 +21,9 @@ coin_chances = [3/5, 2/5]
 samples_amount = 1_000_000
 
 coin_results = random.choices(
-population=list(coin_options.keys()),
-weights=coin_chances,
-k=samples_amount
+    population=list(coin_options.keys()),
+    weights=coin_chances,
+    k=samples_amount
 )
 
 # Parseando pelos resultados
@@ -31,16 +33,16 @@ result_tracker = ["", "", ""]
 
 for coin_result in coin_results:
 
-result_tracker[index] = coin_result
+    result_tracker[index] = coin_result
 
-# TODO: muito feio isso
-index += 1
-if (index == 3):
-contabilize_results(cases, result_tracker)
-result_tracker = ["", "", ""]
-index = 0
+    # TODO: muito feio isso
+    index += 1
+    if (index == 3):
+        contabilize_results(cases, result_tracker)
+        result_tracker = ["", "", ""]
+        index = 0
 
-# Exibição dos resultados
+    # Exibição dos resultados
 
 for key, value in cases.items():
-print(f"{key}: {value}")
+    print(f"{key}: {value}")
